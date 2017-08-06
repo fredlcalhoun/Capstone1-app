db160ecc23d24ef715cef557b5fd7dba57292078
Fred Calhoun <fcalhoun4@yahoo.com>
2017-08-06 15:06:24 -0500

    test capstone1 app

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get "/users" => "users#index"
+  get "/users/new" => "users#new"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
