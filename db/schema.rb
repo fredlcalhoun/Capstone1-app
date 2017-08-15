cbc0ea0b125731fc5ea26e31d50eeac99a871a2f
Fred Calhoun <fcalhoun4@yahoo.com>
2017-08-15 17:55:15 -0500

    capstone1 seed 1st run

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806191636) do
+ActiveRecord::Schema.define(version: 20170806214731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.integer "pet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "lost_or_found"
+    t.string "name"
+    t.string "animal"
+    t.string "breed"
+    t.string "color"
+    t.string "city"
+    t.string "state"
+    t.string "neighborhood"
+    t.string "description"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "user_id"
